80a18be1862110f65247ebcf8f89ae9103d5a666
TacoOtter123 <39072759+TacoOtter123@users.noreply.github.com>
2019-03-15 10:18:58 -0700

    Update Processer.py

--- Processer.py
+++ Processer.py
@@ -5,11 +5,12 @@ import speech_recognition as sr
 import pyaudio
 import wave
 
-r = sr.Recognizer()
-ricky = sr.AudioFile('D:\\Py\\asdf\\demo.wav') #COMPUTER SPECIFIC CHANGE ME
-with ricky as source:
-    audio = r.record(source)
-try:
-    print(r.recognize_google(audio)) 
-except sr.UnknownValueError:
-    print("No sound was captured")
+while True:
+    r = sr.Recognizer()
+    ricky = sr.AudioFile('D:\\Py\\asdf\\demo.wav') #COMPUTER SPECIFIC CHANGE ME
+    with ricky as source:
+        audio = r.record(source)
+    try:
+        print(r.recognize_google(audio)) 
+    except sr.UnknownValueError:
+        print("No sound was captured")
