ce6d5fdc24dc2e9748a88600ec2e9301f573c36c
TacoOtter123 <39072759+TacoOtter123@users.noreply.github.com>
2019-03-16 23:21:57 -0700

    Update ObjectDetection.py
    Works now

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -1,28 +1,32 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('D:\\Py\\xml\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('D:\\Py\\xml\\haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('C:\\Users\\Connor\\Documents\\Code\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:\\Users\\Connor\\Documents\\Code\\haarcascade_eye.xml')
 
 #img = cv2.imread('C:\\Users\\Connor\\Desktop\\PyShots\\felix.jpg')
 cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    frame = np.full((100,80,3), 12, np.uint8)
+    frame = np.array(frame, dtype=np.uint8)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+    for (x, y, w, h) in faces:
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+
     # Display the resulting frame
-        cv2.imshow('frame',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
